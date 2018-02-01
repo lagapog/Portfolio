c09d2010ad7ebc2463ac99c3d888416d118b11a7
Luis Agapito <agapo.91@gmail.com>
2018-02-01 10:46:27 -0500

    Improve meta description/meta theme-color

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,13 +9,15 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'My personal webpage on Nuxt.js' }
+      { hid: 'description', name: 'description', content: '¿Estás empezando en abrir tu propio negocio en línea? ¿Tienes ideas o proyectos por desarrollar y no sabes cómo empezar? ¡Déjame darte una mano!' },
+      { name: 'theme-color', content: "#093140" }
     ],
     link: [
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Raleway' },
       { rel: 'shorcut icon', type: 'image/x-icon', href: 'favicon.ico' },
       { rel: 'apple-touch-icon', sizes: '57x57', href: '/apple-touch-icon-57x57.png' },
-      { rel: 'apple-touch-icon', sizes: '114x114', href: '/apple-touch-icon-114x114.png' }
+      { rel: 'apple-touch-icon', sizes: '114x114', href: '/apple-touch-icon-114x114.png' },
+      { rel: 'icon', sizes: '180x180', href: '/touch-icon-180x180.png' }
     ]
   },
   modules: [
